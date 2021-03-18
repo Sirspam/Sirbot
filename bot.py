58723e8a1e2e963eb0edc63330f27a775ea78e18
Sirspam <Charlespattison555@Gmail.com>
2021-03-18 14:49:07 +0000

    Added firebase credentials to env
    Forgot that heroku can't use the json

--- bot.py
+++ bot.py
@@ -10,7 +10,18 @@ from utils import jskp
 cwd = os.getcwd()
 load_dotenv(f"{cwd}/config.env")
 
-cred = credentials.Certificate(f"{cwd}/sirbot-ede0f-firebase-adminsdk-gtv4c-f1fbbcf054.json")
+cred = credentials.Certificate({
+  "type": "service_account",
+  "project_id": "sirbot-ede0f",
+  "private_key_id": "f1fbbcf05486794a193588e3bf07e1b77c784e18",
+  "private_key": os.getenv("PRIVATE_KEY").replace('\\n', '\n'),
+  "client_email": "firebase-adminsdk-gtv4c@sirbot-ede0f.iam.gserviceaccount.com",
+  "client_id": "110941093760249580602",
+  "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+  "token_uri": "https://oauth2.googleapis.com/token",
+  "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
+  "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-gtv4c%40sirbot-ede0f.iam.gserviceaccount.com"
+})
 firebase_admin.initialize_app(cred)
 
 intents = discord.Intents.default()
