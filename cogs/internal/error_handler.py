941348d1617b85c6187c8434e268293209201c34
Sirspam <Charlespattison555@Gmail.com>
2021-05-05 23:44:37 +0100

    Removed error handler responses deleting

--- cogs/internal/error_handler.py
+++ cogs/internal/error_handler.py
@@ -12,41 +12,41 @@ class ErrorHandler(commands.Cog):
         logging.info(f"on_command_error triggered")
         
         if isinstance(error, commands.BadArgument):
-            logging.info("BadArgument handler ran\n----------")
-            return await ctx.send(f"You've given a bad argument!\nCheck ``{ctx.prefix}help`` for what arguments you need to give", delete_after=20)
+            logging.info("BadArgument handler ran")
+            return await ctx.send(f"You've given a bad argument!\nCheck ``{ctx.prefix}help`` for what arguments you need to give")
 
         elif isinstance(error, commands.CommandNotFound):
-            logging.info("CommandNotFound handler ran\n----------")
-            return await ctx.send("Command not found", delete_after=20)
+            logging.info("CommandNotFound handler ran")
+            return await ctx.send("Command not found")
 
         elif isinstance(error, commands.BotMissingPermissions):
-            logging.info(f"BotMissingPermissions handler ran - {error.missing_perms}\n----------")
+            logging.info(f"BotMissingPermissions handler ran - {error.missing_perms}")
             return await ctx.send(f"Bot missing the following permissions: {error.missing_perms}")
 
         elif isinstance(error, commands.NotOwner):
-            logging.info("NotOwner handler ran\n----------")
-            return await ctx.send("This is an owner only command.", delete_after=20)
+            logging.info("NotOwner handler ran")
+            return await ctx.send("This is an owner only command.")
 
         elif isinstance(error, commands.CommandOnCooldown):
-            logging.info("CommandOnCooldown handler ran\n----------")
+            logging.info("CommandOnCooldown handler ran")
             return await ctx.send(f"Command on cooldown, ``{math.ceil(error.retry_after)} seconds``", delete_after=int(math.ceil(error.retry_after)))
 
         elif isinstance(error, commands.MissingRequiredArgument):
-            logging.info(f"MissingRequiredArgument handler ran. Missing: {error.param.name}\n----------")
-            return await ctx.send("You didn't give a required argument.", delete_after=20)
+            logging.info(f"MissingRequiredArgument handler ran. Missing: {error.param.name}")
+            return await ctx.send("You didn't give a required argument.")
 
         elif isinstance(error, commands.MissingPermissions):
-            logging.info("MissingPermissions handler ran\n----------")
-            return await ctx.send("You don't have the permissions for this command.", delete_after=20)
+            logging.info("MissingPermissions handler ran")
+            return await ctx.send("You don't have the permissions for this command.")
         
         elif isinstance(error, commands.NSFWChannelRequired):
-            logging.info("NSFWChannelRequired hander ran\n----------")
+            logging.info("NSFWChannelRequired hander ran")
             return await ctx.reply("How lewd of you <:AYAYAFlushed:822094723199008799>\n``This command can only be ran in an nsfw channel``")
 
         elif isinstance(error, commands.CheckFailure):
-            logging.error(f"{error}\n----------")
+            logging.error(f"{error}")
 
-        logging.error(f"{error}\n----------")
+        logging.error(f"{error}")
 
 
 def setup(bot):
