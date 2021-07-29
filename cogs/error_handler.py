e9c081fb4e73a0b8276eda843a3fc6c11d312fac
Sirspam <71392316+Sirspam@users.noreply.github.com>
2021-07-29 02:48:36 +0100

    Improved error_handler.py and responses

--- cogs/error_handler.py
+++ cogs/error_handler.py
@@ -6,6 +6,12 @@ from discord import Embed, Colour
 
 from discord.ext import commands
 
+def ErrorEmbed(title: str, description: str) -> Embed:
+    return Embed(
+        title=title,
+        description=description,
+        colour=Colour.red()
+    )
 
 class ErrorHandler(commands.Cog):
     def __init__(self, bot):
@@ -20,49 +26,52 @@ class ErrorHandler(commands.Cog):
         
         if isinstance(error, commands.BadArgument):
             logging.info("BadArgument handler ran")
-            return await ctx.send(f"You've given a bad argument!\nCheck ``{ctx.prefix}help`` for what arguments you need to give")
-
-        if isinstance(error, commands.CommandNotFound):
-            logging.info("CommandNotFound handler ran")
-            return await ctx.send("Command not found")
+            return await ctx.reply(embed=ErrorEmbed("Bad Argument",f"Check ``{ctx.prefix}help`` for what arguments you need to give"))
 
         if isinstance(error, commands.BotMissingPermissions):
             logging.info(f"BotMissingPermissions handler ran - {error.missing_perms}")
-            return await ctx.send(f"Bot missing the following permissions: {error.missing_perms}")
-
-        if isinstance(error, commands.NotOwner):
-            logging.info("NotOwner handler ran")
-            return await ctx.send("This is an owner only command.")
+            return await ctx.reply(embed=ErrorEmbed("Bot Missing Permissions",error.missing_perms))
 
         if isinstance(error, commands.CommandOnCooldown):
             logging.info("CommandOnCooldown handler ran")
-            message = await ctx.send(f"Command on cooldown, ``{ceil(error.retry_after)} seconds``")
+            message = await ctx.reply(embed=ErrorEmbed("Command on Cooldown",f"{ceil(error.retry_after)} seconds"))
             await sleep(int(ceil(error.retry_after)))
-            return await message.add_reaction("✅")
+            return await message.edit(embed=Embed(
+                title="Cooldown Finished",
+                colour=Colour.green()
+            ))
 
         if isinstance(error, commands.MissingRequiredArgument):
             logging.info(f"MissingRequiredArgument handler ran. Missing: {error.param.name}")
-            return await ctx.send("You didn't give a required argument.")
+            return await ctx.reply(embed=ErrorEmbed("Missing Required Arguments",error.param.name))
 
         if isinstance(error, commands.MissingPermissions):
             logging.info("MissingPermissions handler ran")
-            return await ctx.send("You don't have the permissions for this command.")
+            return await ctx.reply(embed=ErrorEmbed("Missing Permissions","You don't have the permissions for this command"))
 
         if isinstance(error, commands.NSFWChannelRequired):
             logging.info("NSFWChannelRequired hander ran")
             return await ctx.reply("How lewd of you <:AYAYAFlushed:822094723199008799>\n``This command can only be ran in an nsfw channel``")
 
         logging.error(error)
+        github_issue_url = ""
+        if self.bot.github_repo:
+            github_issue_url = f"open an [issue report]({self.bot.github_repo}/issues) or"
         await ctx.send(embed=Embed(
-            title="Uh oh. Something bad happened <:NotLikeAqua:822089498866221076>",
-            description=f"An unhandled error occured.\nIf this keeps occuring open an [issue report](https://github.com/Sirspam/Coordy-McCoordFace/issues) or go pester Sirspam <:AquaSmile:845802697474441236>\n\n```{error}```",
+            title="Uh oh. An unhandled error occured ☹️",
+            description=f"If this keeps occuring {github_issue_url} go pester {self.bot.get_user(self.bot.owner_id).name}\n\n```{error}```",
             colour=Colour.red()
         ))
-        return await self.bot.get_channel(841306797985234954).send(embed=Embed(
-            title=f"{ctx.command} in {ctx.guild.name}",
-            description=f"{ctx.guild.id}\n**Message Content**```{ctx.message.content}```\n**Error**```{error}```",
-            colour=Colour.red()
-        ))
+        if self.bot.logging_channel_id:
+            return await self.bot.get_channel(self.bot.logging_channel_id).send(embed=Embed(
+                title=f"{ctx.command} in {ctx.guild.name}",
+                description=
+                f"""**Guild ID**```{ctx.guild.id}```
+                **Author ID**```{ctx.author.id}```
+                **Message Content**```{ctx.message.content}```
+                **Error**```{error}```""",
+                colour=Colour.red()
+            ))
 
 
 def setup(bot):
