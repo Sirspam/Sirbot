164eafc6213a4ded130ca5d1f2f7b36678852cc8
Sirspam <71392316+Sirspam@users.noreply.github.com>
2021-07-31 22:57:32 +0100

    Changed error_handler embed responses

--- cogs/error_handler.py
+++ cogs/error_handler.py
@@ -13,7 +13,7 @@ def ErrorEmbed(title: str, description: str) -> Embed:
         colour=Colour.red()
     )
 
-class ErrorHandler(commands.Cog):
+class CommandErrorHandler(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
@@ -26,7 +26,11 @@ class ErrorHandler(commands.Cog):
         
         if isinstance(error, commands.BadArgument):
             logging.info("BadArgument handler ran")
-            return await ctx.reply(embed=ErrorEmbed("Bad Argument",f"Check ``{ctx.prefix}help`` for what arguments you need to give"))
+            return await ctx.reply(embed=ErrorEmbed("Bad Argument",f"You've given a bad argument for {ctx.command}")) # Response could be better
+
+        if isinstance(error, commands.MissingRequiredArgument):
+            logging.info(f"MissingRequiredArgument handler ran")
+            return await ctx.reply(embed=ErrorEmbed("Missing Required Arguments"f"{ctx.command} requires the {error.param.name} argument\nCheck ``{ctx.prefix}help {ctx.command}`` for more help"))
 
         if isinstance(error, commands.BotMissingPermissions):
             logging.info(f"BotMissingPermissions handler ran - {error.missing_perms}")
@@ -41,38 +45,33 @@ class ErrorHandler(commands.Cog):
                 colour=Colour.green()
             ))
 
-        if isinstance(error, commands.MissingRequiredArgument):
-            logging.info(f"MissingRequiredArgument handler ran. Missing: {error.param.name}")
-            return await ctx.reply(embed=ErrorEmbed("Missing Required Arguments",error.param.name))
-
         if isinstance(error, commands.MissingPermissions):
             logging.info("MissingPermissions handler ran")
             return await ctx.reply(embed=ErrorEmbed("Missing Permissions","You don't have the permissions for this command"))
 
         if isinstance(error, commands.NSFWChannelRequired):
             logging.info("NSFWChannelRequired hander ran")
-            return await ctx.reply("How lewd of you <:AYAYAFlushed:822094723199008799>\n``This command can only be ran in an nsfw channel``")
-
-        logging.error(error)
+            return await ctx.reply(embed=ErrorEmbed("How lewd of you 😳",f"{ctx.command} can only be ran in an nsfw channel"))
         github_issue_url = ""
         if self.bot.github_repo:
             github_issue_url = f"open an [issue report]({self.bot.github_repo}/issues) or"
         await ctx.send(embed=Embed(
             title="Uh oh. An unhandled error occured ☹️",
-            description=f"If this keeps occuring {github_issue_url} go pester {self.bot.get_user(self.bot.owner_id).name}\n\n```{error}```",
+            description=f"If this keeps occuring {github_issue_url} go pester {self.bot.get_user(self.bot.owner_id).mention}\n\n```py\n{error}```",
             colour=Colour.red()
         ))
         if self.bot.logging_channel_id:
             return await self.bot.get_channel(self.bot.logging_channel_id).send(embed=Embed(
-                title=f"{ctx.command} in {ctx.guild.name}",
+                title=f"Unhandled error raised by {ctx.command}",
                 description=
                 f"""**Guild ID**```{ctx.guild.id}```
                 **Author ID**```{ctx.author.id}```
                 **Message Content**```{ctx.message.content}```
-                **Error**```{error}```""",
-                colour=Colour.red()
+                **Error**```py\n{error}```""",
+                colour=Colour.red(),
+                url=ctx.message.jump_url
             ))
 
 
 def setup(bot):
-    bot.add_cog(ErrorHandler(bot))
+    bot.add_cog(CommandErrorHandler(bot))
