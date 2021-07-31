f317147a4c7e627df71324918478450ab881f541
Sirspam <71392316+Sirspam@users.noreply.github.com>
2021-07-31 23:12:04 +0100

    Changed dif_emotes

--- utils/emotes.py
+++ utils/emotes.py
@@ -1,7 +1,7 @@
 diff_emotes = { # Emotes from Sirver
-    "easy": "<:Easy:870900120592461864>",
-    "normal": "<:Normal:870900098790461550>",
-    "hard": "<:Hard:870900076426444800>",
-    "expert": "<:Expert:870900109750185984>",
-    "expertPlus": "<:ExpertPlus:870897674893819984>",
+    "easy": "<:Easy:871152379951710248> ",
+    "normal": "<:Normal:871152579919372290> ",
+    "hard": "<:Hard:871152563666448446>",
+    "expert": "<:Expert:871152394166231072>",
+    "expertPlus": "<:ExpertPlus:871152943523565598>",
 }
