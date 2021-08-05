3ce313905f33438bd5c20a9135ffdd7c3ad3d8e2
Sirspam <71392316+Sirspam@users.noreply.github.com>
2021-08-05 14:43:41 +0100

    Added diff_stats emotes

--- utils/emotes.py
+++ utils/emotes.py
@@ -1,7 +1,15 @@
 diff_emotes = { # Emotes from Sirver
-    "easy": "<:Easy:871152379951710248> ",
-    "normal": "<:Normal:871152579919372290> ",
-    "hard": "<:Hard:871152563666448446>",
-    "expert": "<:Expert:871152394166231072>",
-    "expertPlus": "<:ExpertPlus:871152943523565598>",
+    "Easy": "<:Easy:871152379951710248> ",
+    "Normal": "<:Normal:871152579919372290> ",
+    "Hard": "<:Hard:871152563666448446>",
+    "Expert": "<:Expert:871152394166231072>",
+    "ExpertPlus": "<:ExpertPlus:871152943523565598>",
+}
+
+diff_stats_emotes = {
+    "star": "⭐",
+    "notes": "<:notes:872053040184700958>",
+    "bombs": "<:bombs:872151164895002665>",
+    "nps": "<:nps:872151181596692550>",
+    "njs": "<:njs:872151173849825321>"
 }
